ef911acfe21d4a4d51b195a1e5d77e0a0ebde85c
bob9292 <sunghan1630@naver.com>
2025-02-25 21:14:52 +0900

    게시물 상세페이지 기능 구현, 날짜 아직

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -50,6 +50,32 @@ public class Main {
 
         System.out.printf("%d번 글이 작성되었습니다.\n", id);
       }
+      else if ( cmd.startsWith("article detail ")) {
+        String[] cmdBits = cmd.split(" ");
+        int id = Integer.parseInt(cmdBits[2]);
+
+        Article foundArticle = null;
+
+        for ( int i = 0; i < articles.size(); i++){
+          Article article = articles.get(i);
+
+          if ( article.id == id ) {
+            foundArticle = article;
+            break;
+          }
+        }
+
+        if ( foundArticle == null ) {
+          System.out.printf("%d번 게시물은 존재하지 않습니다.\n",id);
+          continue;
+        }
+
+        System.out.printf("번호 : %d\n", foundArticle.id);
+        System.out.printf("날짜 : %s\n", "2025.02.22");
+        System.out.printf("제목 : %s\n", foundArticle.title);
+        System.out.printf("내용 : %s\n", foundArticle.body);
+      }
+
     }
     sc.close();
 
